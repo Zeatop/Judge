82137cda7fec54a743eb02d18cd17a2058311dc9
léo jackson <leojackson@MacBook-Air-de-leo.local>
2026-03-30 20:57:52 +0200

    fix card preview

--- api.py
+++ api.py
@@ -31,7 +31,9 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # port Vite par défaut
+    allow_origins=["http://localhost:5173",
+                   "http://localhost:5174",
+                   "http://localhost:5175",],  # port Vite par défaut
     allow_methods=["*"],
     allow_headers=["*"],
 )
